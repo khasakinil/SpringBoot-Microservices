181802c068dd231fd1aaad8bd9c79c806a9074cc
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-03-25 00:35:43 +0530

    Spring Cloud V1
    Limits Service added

---
+++ limits-service/src/main/java/com/microservices/v1/config/LimitsConfigurationProperties.java
@@ -0,0 +1,29 @@
+package com.microservices.v1.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class LimitsConfigurationProperties {
+
+	private int maximum;
+	private int minimum;
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+}
